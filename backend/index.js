c2b9a066e2fa8a0d49e3d6a2e257c9b7a772e10d
Lucas Mauro <lucas_de_mauro@hotmail.com>
2020-03-24 12:34:50 -0300

    Node.js e Express

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,34 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
+app.use(express.json());
+
+/**
+ * Rota / Recurso
+ */
+
+/**
+ * Metodos HTTP:
+ * 
+ * GET: Buscar uma informação do backend
+ * POST: Criar uma informação do backend
+ * PUT: Alterar uma informação do backend
+ * DELETE: Deletar uma informação do backend
+ */
+
+/**
+ * Tipos de parâmetros:
+ * 
+ * Query Params: Parâmetros nomeados enviados na rota após "?" (Filtros, paginação)
+ * Route Params: Parâmetros utilizados para identificar recursos
+ * Request Body: Corpo da requisição, utilizado para criar ou alterar recursos
+ */
+
+app.post('/users', (request, response) => {
+    const body = request.body;
+
+    console.log(body);
+
     return response.json({
         evento: 'Semana Omnistack 11.0',
         aluno: 'Lucas Mauro'
